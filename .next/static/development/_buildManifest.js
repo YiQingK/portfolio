fa6949890263c4a70405a442a47256c4e9d1775f
YiQingK <khooyiqing@gmail.com>
2023-10-11 19:04:16 -0700

    removed .next folder from git

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/work/h2games":["static\u002Fchunks\u002Fpages\u002Fwork\u002Fh2games.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fwork\u002Fh2games"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
