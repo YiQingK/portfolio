9b00f2394f1260700612ada39358a0a7088a78d6
YiQingK <khooyiqing@gmail.com>
2024-02-27 17:02:26 +0800

    updated social hero and added competitive audit images

--- src/pages/works/socialhero.js
+++ src/pages/works/socialhero.js
@@ -5,9 +5,10 @@ import Image from "next/image";
 import Link from "next/link";
 import habiticapic from "../../../public/images/habitica.png";
 import leveluplifepic from "../../../public/images/leveluplife.png";
+import habiticalight from "../../../public/images/habitica_light.png";
+import leveluplifelight from "../../../public/images/leveluplife_light.png";
 import codesign1 from "../../../public/images/codesign1.jpg";
 import codesign2 from "../../../public/images/codesign2.jpg";
-import venndiagram from "public/images/Venn.png";
 import wireframe from "public/images/socialhero_wireframe.png";
 import landingpage from "public/images/landingpage_phone.png";
 import tavern1 from "public/images/tavern_phone.png";
@@ -26,11 +27,12 @@ const socialhero = () => {
 				{/*flex w-full flex-col items-center justify-center dark:text-light*/}
 				<Layout>
 					<h5 className=" bg-primary dark:bg-primaryDark text-light inline p-1 rounded">UX Research</h5>
+					<h5 className=" bg-primary dark:bg-primaryDark text-light inline p-1 rounded ml-4">UX Design</h5>
 					<div className="grid grid-cols-12 gap-12 gap-y-24 md:gap-8">
 						<div id="title" className="col-span-12">
 							<h1>Social Hero</h1>
 							<h2>Fostering socialization through mobile games</h2>
-							<div className="mt-12 w-full flex py-12 md:flex-col md:text-center bg-primary bg-opacity-25">
+							<div className="mt-12 w-full flex gap-12 py-12 md:flex-col md:text-center border-b-2 border-black dark:border-light">
 								<div className="w-[50%] md:w-full md:px-4">
 									<h4>Overview</h4>
 									<p>
@@ -62,39 +64,70 @@ const socialhero = () => {
 						<div id="userinterview" className="col-span-12">
 							<h3 className="font-bold">User Interview</h3>
 							<p>
-								Exploratory user interviews which took 30-45 minutes were conducted to understand
-								potential users’ thoughts on what are good features by asking about their expectations
-								of such a game. Participants were graduate students These user interviews had opened a
-								new avenue of thought - to foster social courage rather than to ‘fix social anxiety’.
+								We had conducted exploratory user interviews with 3 graduate students who had identified
+								as introverts that also enjoy games. Through these interviews, we wanted to introduce
+								the idea of a gamified app that can help "fix" social anxiety by suggesting players task
+								they can do in the real world and get feedback from the interviewees about how they see
+								themselves using the application and some features that they would like to see.
+							</p>
+							<p>
+								From these interviews, we heard details such as "" and it had opened a new avenue of
+								thought - to foster social courage rather than to ‘fix social anxiety’.
 							</p>
 						</div>
 						<div id="refinement" className="col-span-12">
 							<h3 className="font-bold">Problem Statement Refinement</h3>
 							<p>
 								With the new perspective introduced from the user interivew, we pivoted our research
-								to...
+								from just looking at introverts to any who might want to be more sociable.
 							</p>
 						</div>
 						<div id="deepdive" className="col-span-12">
 							<h3 className="font-bold">Deep dive into online communities</h3>
-							<h4>Online Survey</h4>
 							<p>
-								We had successfully surveyed over 200 participants (demographics, how it was set up for
-								success). This survey was aimed to better understand how people navigate online spaces,
-								learning about their experiences and understanding what makes them feel safe.
+								Looking to better understand how people navigate online spaces, learn about their
+								experiences and understanding what makes them feel safe, we decided to release an online
+								survey and follow with an in-person workshop with willing participants.
 							</p>
-							<h4>In-person Workshop</h4>
+							<h4 className="mt-8">Online Survey</h4>
 							<p>
-								Through recruitment from the online survey, we conducted an in-person workshop which
-								consisted of 2 parts - a focus group discussion session and a co-design session. Each
-								workshop consisted of 4-6 participants, with 1 team member leading each part.
-								<br /> <br />
-								The <span className="font-bold">focus groups</span> allowed participants share their own
-								experiences while also potentially relating to stories of other participants.
-								<br /> <br />
-								The <span className="font-bold">co-design session</span> had participants design screens
-								they would like to see in our game. This allowed us to understand features they’d like
-								in the game as well as their reasoning behind it.
+								We had successfully surveyed over 200 participants with age ranging from 18-43, majority
+								being university students. We had released the survey on social media as well as share
+								it through the university network.
+								<br></br>
+								<br></br>
+								Many respondents shared that they are motivated to continue using online spaces due to
+								the community aspect as well as communicating with others. Participants also shared that
+								having control of who they can talk to contributes to them feeling safe online.
+							</p>
+							<h4 className="mt-8">In-person Workshop</h4>
+							<p>
+								The in-person workshop consisted of 2 parts - a focus group discussion and a co-design
+								session. Each workshop consisted of 4-6 participants, with 1 team member leading each
+								part.
+							</p>
+							<h5 className="mt-8">Part 1: Focus Group</h5>
+							<p>
+								During the focus group, we introduced different scenarios, each looking into a different
+								aspects of users in online spaces including community aspects that contribute to the
+								feeling of safety, online activity influencing offline activity and their experinces in
+								using online platforms for social interactions.
+								<br></br>
+								<br></br>
+								One thought that was commonly brought up amongst the different groups of workshops was
+								that online spaces is a great place for social interaction and finding others of the
+								same passion. Many agreed that having someone to faciliate interactions is also helpful.
+							</p>
+							<h5 className="mt-8">Part 2: Co-design Session</h5>
+							<p>
+								During the session, we had participants discuss with their neighbour what elements that
+								they have seen/been exposed to that is most effective in making connections on online
+								games. After having them ideate, we had participants to design screens for a mobile game
+								that would help faciliate socialization.
+								<br></br>
+								<br></br>
+								Many participants had drawn screens that showed player interaction in a game world and
+								also in-game chats, working together to complete a goal.
 							</p>
 						</div>
 						<div className="col-span-7 md:col-span-12">
@@ -105,34 +138,12 @@ const socialhero = () => {
 							<Image src={codesign2} className="rounded-lg border-2 border-solid border-dark" />
 							<p>Example of co-design session activity</p>
 						</div>
-						<div id="competitiveaudit" className="col-span-12">
-							<h3 className="font-bold">Competitive Audit</h3>
-							<p>
-								A competitive audit was conducted on applications that were deemed similar to our idea,
-								to understand features that are already successful with users. These applications
-								include <span className="text-primary dark:text-primaryDark">‘Habitica’</span> and
-								<span className="text-primary dark:text-primaryDark">‘Level-up Life’</span>. We
-								play-tested and collected app store reviews to understand the successful aspects of
-								these apps.
-							</p>
-							<div className="flex md:flex-col gap-12 md:gap-8">
-								<div className="text-center w-[50%] md:w-full">
-									<Image src={habiticapic} className=" w-auto max-h-[700px] " />
-									<p className="max-w-[700px]">Screenshot of Habitica</p>
-								</div>
-								<div className="text-center w-[50%]  md:w-full">
-									<Image src={leveluplifepic} className="w-auto max-h-[700px]" />
-									<p className="max-w-[700px]">Screenshot of Level Up Life</p>
-								</div>
-							</div>
-						</div>
-
 						<div id="findings" className="col-span-12">
 							<h3 className="font-bold">Research Findings and Features</h3>
-							<h4>
-								Data from the various methods were brought together and the below major themes were
-								created.
-							</h4>
+							<p>
+								Through the interview, online survey and in-person workshop, the following major themes
+								were defined.
+							</p>
 							<div className="flex mt-12 gap-12 md:flex-col">
 								<div className="w-[50%] md:w-full border-2 border-dark rounded-2xl p-2 dark:border-light">
 									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDark">
@@ -142,7 +153,7 @@ const socialhero = () => {
 									<p>Collaboration between users</p>
 								</div>
 								<div className="w-[50%] md:w-full border-2 border-dark rounded-2xl p-2 dark:border-light">
-									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDar">
+									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDark">
 										Safety
 									</h5>
 									<p>Control of who they are interacting with</p>
@@ -151,14 +162,14 @@ const socialhero = () => {
 							</div>
 							<div className="flex mt-12 gap-12 md:flex-col">
 								<div className="w-[50%] md:w-full border-2 border-dark rounded-2xl p-2 dark:border-light">
-									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDar">
+									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDark">
 										Aesthetics
 									</h5>
 									<p>Reminiscent and colorful 8-bit art style</p>
 									<p>World style differing from real life (Escapism)</p>
 								</div>
 								<div className="w-[50%] md:w-full border-2 border-dark rounded-2xl p-2 dark:border-light">
-									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDar">
+									<h5 className=" underline underline-offset-4 text-primary dark:text-primaryDark">
 										User
 									</h5>
 									<p>Ability to express themselves</p>
@@ -166,21 +177,39 @@ const socialhero = () => {
 								</div>
 							</div>
 						</div>
-						<div id="brainstorming" className="col-span-12">
-							<ul className="list-disc marker:text-primary">
-								<h3 className="font-bold">Brainstorming</h3>
-							</ul>
-							<h4>
-								Based off research findings, we brainstormed for features as shown in the venn diagram.
-							</h4>
-							<p>Each venn circle represents each designers ideas</p>
-							<Image
-								src={venndiagram}
-								className=" w-auto max-h-[500px] rounded-lg border-2 border-solid border-dark p-4"
-							/>
+						<div id="competitiveaudit" className="col-span-12">
+							<h3 className="font-bold">Competitive Audit</h3>
+							<p>
+								A competitive audit was conducted on applications that gamified the experiences of doing
+								tasks to understand features that are already successful with users as well as areas of
+								improvements that could be applied to the design we were building. These applications
+								include <span className="text-primary dark:text-primaryDark">‘Habitica’</span> and
+								<span className="text-primary dark:text-primaryDark">‘Level-up Life’</span>. For this
+								competitive audit, we synthesized app store reviews as well as play-tested the
+								applications.
+							</p>
+							<div className=" flex flex-col gap-12 md:gap-8 mt-12">
+								<div className=" w-full">
+									<h4>Aspects of Habitica</h4>
+									<Image src={habiticapic} className=" w-auto max-h-[700px] mt-4 dark:hidden" />
+									<Image
+										src={habiticalight}
+										className="w-auto max-h-[700px] mt-4 hidden dark:block"
+									/>
+								</div>
+								<div className="w-full">
+									<h4>Aspects of Level Up Life</h4>
+									<Image src={leveluplifepic} className="w-auto max-h-[700px] mt-4 dark:hidden" />
+									<Image
+										src={leveluplifelight}
+										className="w-auto max-h-[700px] mt-4 hidden dark:block"
+									/>
+								</div>
+							</div>
 						</div>
 						<div id="Finalized Features" className="col-span-12">
 							<h3>Finalized Features</h3>
+							<p></p>
 							<ul className="list-disc">
 								<li className="list-item">
 									Separated spaces for close friends and all players with the option to be set
@@ -199,6 +228,20 @@ const socialhero = () => {
 								</li>
 							</ul>
 						</div>
+						<div id="concepts" className="col-span-12">
+							<h3 className="mt-4">Concept Designs</h3>
+							<p>
+								Each designer took it on thei own to create different concepts coming up with the
+								modern, fantasy and farm concepts.
+							</p>
+							<h4>Concept 1 - Modern</h4>
+							<h4>Concept 2 - Fantasy</h4>
+							<h4>Concept 3 - Farm</h4>
+							<p>
+								We eventually decided on a mix of fantasy and farm, taking in the brighter colors of the
+								farm concept whilst keeping the escapsim/part from reality idea of fantasy.
+							</p>
+						</div>
 						<div id="wireframe" className="col-span-12">
 							<h3 className="mt-4">Wireframe</h3>
 							<Image src={wireframe} className="w-auto max-h-[700px] m-auto" />
@@ -207,7 +250,10 @@ const socialhero = () => {
 							<h3 className="mt-4">Usability Testing</h3>
 							<p></p>
 						</div>
-						<div id="hifiprototype" className="col-span-12">
+						<div id="usability findings" className="col-span-12">
+							<h3>Usability Testing Findings</h3>
+						</div>
+						<div id="hifidesign" className="col-span-12">
 							<h3 className="mt-4">High Fidelity Design</h3>
 							<h4>Landing Page</h4>
 							<p>The guide was added to help first time users better navigate the application.</p>
@@ -231,6 +277,9 @@ const socialhero = () => {
 							<h4>Academy Log</h4>
 							<p>Academy log showcases achievements and tasks-related posts with in-game friends only.</p>
 							<Image src={academy} className="w-auto max-h-[700px] m-auto" />
+						</div>
+						<div id="outcome" className="col-span-12">
+							<h3>If more time</h3>
 						</div>
 					</div>
 				</Layout>
